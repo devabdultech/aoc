b7cc06c1bb6841a094f26e1d23dc4671fdc58c90
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-12-03 16:55:08 +0100

    🎄 Day One Solved

--- 2023/day_01/python/solution.py
+++ 2023/day_01/python/solution.py
@@ -1,1 +1,13 @@
-print("🎄")
+import re
+
+cv = 0
+
+with open("./input.txt") as f:
+    lines = f.readlines()
+    for line in lines:
+        digits = re.findall('\d', line)
+
+        cv = cv + int(digits[0] + digits[-1])
+
+print(
+    f"Congratulations! You've successfully recovered the calibration values. The sum of all values is: {cv} stars! 🌟✨")
