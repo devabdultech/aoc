be2e3801b18459cbf00906bd81ac2d2cb412a599
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-12-06 21:44:31 +0100

    🎄 Day 2 (Part 1) solved

--- 2023/day_02/python/solution.py
+++ 2023/day_02/python/solution.py
@@ -1,1 +1,32 @@
-print("🎄")
+with open("./input.txt") as f:
+    lines = f.read().strip().split("\n")
+
+total = 0
+
+for line in lines:
+    game, cubes = line.split(": ")
+    gameId = int(game.split(" ")[1])
+
+    possible = True
+    for cube in cubes.split("; "):
+        for color in cube.split(", "):
+            num, c = color.split(" ")
+            num = int(num)
+
+            if c == "red" and num > 12:
+                possible = False
+            if c == "blue" and num > 14:
+                possible = False
+            if c == "green" and num > 13:
+                possible = False
+
+            if not possible:
+                break
+
+        if not possible:
+            break
+
+    if possible:
+        total += gameId
+
+print(total)
