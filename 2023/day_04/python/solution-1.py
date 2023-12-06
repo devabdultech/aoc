0199d8c88d7b39adb03e113296f96b482e5ae367
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-12-06 23:04:46 +0100

    🎄 Day 4 (Part 1) solved

--- 2023/day_04/python/solution-1.py
+++ 2023/day_04/python/solution-1.py
@@ -1,1 +1,19 @@
-print("🎄")
+import re
+
+points = 0
+
+with open("./input.txt") as f:
+    lines = [x.strip() for x in f.readlines()]
+
+    for line in lines:
+        (leftNums, rightNums) = line.split("|")
+
+        wNumbers = re.findall(r"\d+", leftNums)[1::]
+        aNumbers = re.findall(r"\d+", rightNums)
+
+        common = set(wNumbers).intersection(aNumbers)
+
+        if len(common) > 0:
+            points += 2 ** (len(common) - 1)
+
+print(points)
