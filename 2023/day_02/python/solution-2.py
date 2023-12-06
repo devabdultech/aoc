7907ce5e655cf73d2463cd6f623ecf1f3f62f62e
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-12-06 21:45:06 +0100

    🎄 Day 2 (Part 2) solved

--- 2023/day_02/python/solution-2.py
+++ 2023/day_02/python/solution-2.py
@@ -1,1 +1,24 @@
-print("🎄")
+with open("./input.txt") as f:
+    lines = f.read().strip().split("\n")
+
+total = 0
+
+for line in lines:
+    game, cubes = line.split(": ")
+    r, g, b = 0, 0, 0
+
+    for cube in cubes.split("; "):
+        for color in cube.split(", "):
+            num, c = color.split(" ")
+            num = int(num)
+
+            if c == "red":
+                r = max(r, num)
+            if c == "blue":
+                b = max(b, num)
+            if c == "green":
+                g = max(g, num)
+
+    total += r * g * b
+
+print(total)
